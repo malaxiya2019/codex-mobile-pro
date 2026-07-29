a5305b26b83c944b74bfafd837c3443dfb5f183d
Liang2050 <liang2050@example.com>
2026-07-29 08:24:35 +0800

    🐛 fix: 移除 environment_service_test.dart 中多余 import

--- test/environment_service_test.dart
+++ test/environment_service_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:codex_mobile_pro/core/detector/environment_service.dart';
-import 'package:codex_mobile_pro/core/termux/termux_service.dart';
 
 void main() {
   const channel = MethodChannel('com.codexmobile.app/termux');
